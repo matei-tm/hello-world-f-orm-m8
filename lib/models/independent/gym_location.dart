3e436dab61919bb18e5f6393f0f84ad506860965
Mircea MATEI <matei_tm@yahoo.com>
2019-06-04 17:47:52 +0300

    #1. Fix format

--- lib/models/independent/gym_location.dart
+++ lib/models/independent/gym_location.dart
@@ -1,15 +1,14 @@
-
 // GENERATED CODE - DO NOT MODIFY THIS HEADER
 // **************************************************************************
-// Generator: vscode-f-orm-m8 
+// Generator: vscode-f-orm-m8
 // Version: 0.1.6
 // Database: Sqlite
 // Timestamp: 1559633795240
 // **************************************************************************
-// 
+//
 // WARNING: If you alter the lines above, on future updates
 //          the extension will skip this file
-// 
+//
 // USER CODE - FROM THIS LINE YOU ARE FREE TO MODIFY THE CONTENT
 //
 // The model respect f-orm-m8 framework annotations system
@@ -18,27 +17,22 @@
 // You are free and also responsible to add your own fields
 //   and annotate according to f-orm-m8
 //
-// If you changed this file you must 
-//   re-run the extension 
+// If you changed this file you must
+//   re-run the extension
 //   f-orm-m8: Generate Sqlite Fixture
 //   from the command pallette
-
-
 
 import 'package:f_orm_m8/f_orm_m8.dart';
 
 @DataTable("gym_locations")
 class GymLocation implements DbEntity {
-  @DataColumn(
-    "id",
-    metadataLevel: ColumnMetadata.primaryKey |
-        ColumnMetadata.autoIncrement)
+  @DataColumn("id",
+      metadataLevel: ColumnMetadata.primaryKey | ColumnMetadata.autoIncrement)
   @override
   int id;
 
   @DataColumn("description", metadataLevel: ColumnMetadata.unique)
   String description;
-
 
   /* The next field is annotated as `ignore`. Feel free to adjust or delete it according to your requirements. */
   @DataColumn("my_future_bigint_column", metadataLevel: ColumnMetadata.ignore)
@@ -71,5 +65,4 @@ class GymLocation implements DbEntity {
   /* The next field is annotated as `ignore`. Feel free to adjust or delete it according to your requirements. */
   @DataColumn("my_future_string_column", metadataLevel: ColumnMetadata.ignore)
   String stringFutureField;
-
 }
